e050b41d9bd128bbd4eaf8130e9284555e9149a8
Kostiantyn Zhereb <zhereb@gmail.com>
2021-02-02 09:47:23 +0200

    Creating array on heap, large arrays are possible

--- src/lesson_2021_02_02_stack_vs_heap.cpp
+++ src/lesson_2021_02_02_stack_vs_heap.cpp
@@ -6,7 +6,7 @@
  */
 #include <iostream>
 
-const std::size_t DATA_SIZE = 100000;
+const std::size_t DATA_SIZE = 10000000;
 
 
 void process_array_on_stack() {
@@ -20,8 +20,20 @@ void process_array_on_stack() {
 	}
 }
 
+
+void process_array_on_heap() {
+	int* data = new int[DATA_SIZE];
+	for (std::size_t i = 0; i<DATA_SIZE; i++) {
+		data[i] = i*5+3;
+	}
+
+	for (std::size_t i = 0; i<DATA_SIZE; i++) {
+		std::cout<<"data["<<i<<"]="<<data[i]<<std::endl;
+	}
+}
+
 int main() {
-	process_array_on_stack();
+	process_array_on_heap();
 	return 0;
 }
 
