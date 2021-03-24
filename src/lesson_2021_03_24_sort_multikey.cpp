8ab28d15b4b47f48baed48a7b8a0c3e6b24b934d
Kostiantyn Zhereb <zhereb@gmail.com>
2021-03-24 09:35:44 +0200

    Implement configurable multikey sort using function pointers

--- src/lesson_2021_03_24_sort_multikey.cpp
+++ src/lesson_2021_03_24_sort_multikey.cpp
@@ -21,8 +21,34 @@ bool compare_x_y(const Point& first, const Point& second) {
 	} else { return first.y < second.y; }
 }
 
+
+
+bool compare_multi_x(const Point& first, const Point& second, bool (*next_compare)(const Point&, const Point&)) {
+	if (first.x != second.x) {
+		return first.x < second.x;
+	} else { return next_compare(first, second); }
+}
+
+bool compare_multi_y(const Point& first, const Point& second, bool (*next_compare)(const Point&, const Point&)) {
+	if (first.y != second.y) {
+		return first.y < second.y;
+	} else { return next_compare(first, second); }
+}
+
+bool compare_x(const Point& first, const Point& second) {
+	return first.x < second.x;
+}
+
+bool compare_y(const Point& first, const Point& second) {
+	return first.y < second.y;
+}
+
+
 void sort_x_y(Point* arr, std::size_t size) {
-	std::sort(arr, arr+size, compare_x_y);
+	//std::sort(arr, arr+size, compare_x_y);
+	std::sort(arr, arr+size, [](const Point& first, const Point& second) {
+		return compare_multi_x(first, second, compare_y);
+	});
 
 }
 
